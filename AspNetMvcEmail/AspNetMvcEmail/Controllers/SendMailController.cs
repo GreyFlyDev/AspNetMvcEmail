4bf26d41b4e3caa74919b340f07fb329177fa583
Greg Flynn <gflynn@covdnssrv.co.volusia.fl.us>
2016-06-07 09:46:46 -0400

    Complete Example. 1 Model 1 Controller 2 Actions

---
+++ AspNetMvcEmail/AspNetMvcEmail/Controllers/SendMailController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcEmail.Controllers
+{
+    public class SendMailerController : Controller
+    {
+        //
+        // GET: /SendMailer/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Index(AspNetMvcEmail.Models.Mail _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("gregnnylf94@gmail.com", "Enjoilif3!");// Enter seders User name and password
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ AspNetMvcEmail/AspNetMvcEmail/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AspNetMvcEmail")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("County of Volusia")]
+[assembly: AssemblyProduct("AspNetMvcEmail")]
+[assembly: AssemblyCopyright("Copyright © County of Volusia 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e372714e-6526-4db0-a016-8d4585b6c5df")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
